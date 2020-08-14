88014ba39039b0c56627289cef2d449f032ee42f
Alexandre Spreafico Novaes <alexandre.novaes@acesso.com>
2020-08-14 10:13:38 -0300

    correção nome do projeto infra

--- src/MrPizza.API/Startup.cs
+++ src/MrPizza.API/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FluentValidation.AspNetCore;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MrPizza.Domain.Infra.Contexts;
 
 namespace MrPizza
